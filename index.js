f69e786d6ce02a84f0f14d6fac02c4569268443d
Alisa Apotovska <masanaapot@gmail.com>
2023-04-06 01:05:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+sum(5, 6);
+function sub(a, b) {
+  const result = a - b;
+  return result;
+}
+sub(5, 6);
+function mult(a, b) {
+  const result = a * b;
+  return result;
+}
+mult(5, 6);
